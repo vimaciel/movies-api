e14c233d2e4de3d6372fb20bf175f35085da375a
vimaciel <viniciusfmaciel@gmail.com>
2019-07-21 20:40:20 -0300

    Update movie endpoint developed

--- routes/movies.js
+++ routes/movies.js
@@ -5,6 +5,7 @@ const successResponse = require('../resources/success-response')
 const errorResponse = require('../resources/error-response')
 const validateBody = require('../middlewares/validate-body')
 const { getGenreAsync } = require('../models/genre')
+const validObjectId = require('../middlewares/validate-object-id')
 
 router.get('/', (_, res) => {
     const movies = Movie.find().sort('title')
@@ -30,6 +31,32 @@ router.post('/', validateBody(validateMovie), async (req, res) => {
         await movie.save()
 
         res.status(400).send(errorResponse(movie))
+    }
+})
+
+router.put('/:id', [validObjectId, validateBody(validateMovie)], async (req, res) => {
+    const movie = await Movie.findById(req.params.id)
+
+    if (!movie) {
+        return res.status(404).send(errorResponse(['Movie not found']))
+    }
+
+    const { title, genre: genreItems, releaseDate, actors, summarizedPlot, youtubeTrailer, posterUrlImage } = req.body
+    const genre = await validateGenreItemsAsync(genreItems, res)
+
+    if (genre) {
+        movie.set({
+            title,
+            genre,
+            releaseDate,
+            actors,
+            summarizedPlot,
+            youtubeTrailer,
+            posterUrlImage
+        })
+
+        await movie.save();
+        res.send(successResponse(movie));
     }
 })
 
